5f6a410b6a96e961e323f29bf1c5f5ee62a049c9
ala-ism <ismail.alaoui@eleven-strategy.com>
2019-11-07 18:17:19 +0100

    ok

--- AI102_Anomaly_Detection.py
+++ AI102_Anomaly_Detection.py
@@ -23,6 +23,19 @@ X_outliers = rng.uniform(low=-1, high=5, size=(50, 2))
 X_outliers = pd.DataFrame(X_outliers, columns = ['x1', 'x2'])
 
 #%%
-colors=('red','green','blue')
-plt.scatter(X_outliers['x1'],X_outliers['x2'])
+g1=(X_outliers['x1'],X_outliers['x2'])
+g2=(X_test['x1'],X_test['x2'])
+g3=(X_train['x1'],X_train['x2'])
+data=(g1,g2)#,g3)
+groups=("outliers","normal")#,"training")
+colors=('red','white')#,'blue')
+fig=plt.figure()
+ax=fig.add_subplot(1,1,1)
+
+for data, color, group in zip(data, colors, groups):
+    x, y=data
+    ax.scatter(x,y, alpha=0.8, c=color, edgecolors='none', s=30, label=group)
+
+plt.title('outlier detection result')
+plt.legend(loc=2)
 plt.show()
