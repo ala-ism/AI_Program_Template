3f64361108ee554e9e95b1fb3ff4af9fcc7ed809
LNBaud <44223448+LNBaud@users.noreply.github.com>
2019-11-18 20:19:42 +0100

    Kmeans comment

--- AI102_K_Means.py
+++ AI102_K_Means.py
@@ -2,21 +2,33 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import cv2
+import numpy as np
 
 #%%
 img=cv2.imread(r"C:/Users/test.jpg")
 img=cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+print(img.shape)
+
+#We convert the picture from a 1666x2235 matrix to a vector
 vectorized = img.reshape((-1,3))
+print(vectorized.shape)
 vectorized = np.float32(vectorized)
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+
 #%%
+#Creation of the K-means model see documentation : https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_ml/py_kmeans/py_kmeans_opencv/py_kmeans_opencv.html
 #try playing with the K parameter and see what happens (e.g., K=2, K=3)
-K = 15
+K = 5
 attempts=10
+
+#Specify iteration termination criteria for K-Means
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+
+
 ret,label,center=cv2.kmeans(vectorized,K,None,criteria,attempts,cv2.KMEANS_PP_CENTERS)
 center = np.uint8(center)
 
-#%%
+#Visualization of the result
 res = center[label.flatten()]
 result_image = res.reshape((img.shape))
 figure_size = 15
@@ -27,3 +39,6 @@ plt.subplot(1,2,2),plt.imshow(result_image)
 plt.title('Segmented Image when K = %i' % K), plt.xticks([]), plt.yticks([])
 plt.show()
 
+
+
+# %%
