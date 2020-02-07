4dd7097b8bc34cffaecff1e3bc3914709b824a52
ala-ism <37927626+ala-ism@users.noreply.github.com>
2020-02-07 16:32:36 +0100

    commit

--- MyOwnRL_Env.py
+++ MyOwnRL_Env.py
@@ -9,19 +9,27 @@ import time
 
 style.use("ggplot")
 # %%
-SIZE=10
-NB_EPISODES=25000
-MOVE_PENALTY=1
-ENEMY_PENALTY=20
-FOOD_REWARD=50
-epsilon=0.5
+SIZE=10 #The environment is a 10x10 sized square
+
+NB_EPISODES=25000 #number of games to be played to train our model
+
+MOVE_PENALTY=1 #everytime the agent move one point is removed from its reward
+
+ENEMY_PENALTY=20 #everytime the agent touches an enemy it looses 20 points
+
+FOOD_REWARD=50 #getting to the food gets you 50 points
+
+epsilon=0.5 #the exploration parameter is set to 0.5
+
 EPSILON_DECAY=0.9 #epsilon will be updated following: epsilon*=EPISOLON_DECAY
-SHOW_EVERY=100
+
+SHOW_EVERY=100 #We do not want to display information on all the games that are played to save memory and make the training faster
 
 start_q_table=None #we could have a pickled qtable here too
 
-LEARNING_RATE=0.1
-DISCOUNT=0.9
+LEARNING_RATE=0.1 #much weight is put on past rather than future information
+
+DISCOUNT=0.9 #the max potential futur reward is highly discounted
 
 #create stakeholders' IDs for the color dictionnary 
 PLAYER_ID=1
@@ -29,20 +37,27 @@ FOOD_ID=2
 ENEMY_ID=3
 
 #colors:
-d={1:(255,175,0), 2:(0,255,0), 3:(0,0,255)}
+d={1:(255,175,0), 2:(0,255,0), 3:(0,0,255)} #setting up the colors for our visual representation
 
 #%%
+#This part consists in creating our agents. To do so, we are creating class objects: 
+## In Python, classes can be though of as specific objects composed of various basic other objects and subject to specific functions and operands.
+## A famous class example in Python would be the pandas.DataFrame class: it is composed of simpler objects and has its own list of functions
 class Blob:
+    #The agent is characterized by its position: an (x,y) couple
     def __init__(self):
         self.x = np.random.randint(0, SIZE)
         self.y = np.random.randint(0, SIZE)
-
+        
+    #This function is our first operator override: we make sure that a print(agent) would print the two coordinates
     def __str__(self):
         return f"{self.x}, {self.y}"
     
+    #This function is the second operator override: substracting two agents amounts to substracting the Xs and the Ys
     def __sub__(self, other):
         return (self.x-other.x, self.y-other.y)
     
+    #Defining the set of move that we authorize our agent to make
     def move(self, x=False, y=False):
         #if there are no values for x or y, move randomly
         if not x:
@@ -66,6 +81,7 @@ class Blob:
         elif self.y>SIZE-1:
             self.y=SIZE-1
     
+    #This functions associates choices to actions
     def action(self, choice):
         #Gives us 4 total movement options. (0,1,2,3)
 
@@ -78,6 +94,7 @@ class Blob:
         elif choice == 3:
             self.move(x=1, y=-1)
 #%%
+#The q-table is randomly initialized
 if start_q_table is None: 
     q_table={}
     for i in range(-SIZE+1, SIZE):
@@ -89,6 +106,7 @@ else:
     with open(start_q-table, 'rb') as f:
         q_table=pickle.lead(f)
 # %%
+#The game is played 
 episode_rewards = []
 
 for episode in range(NB_EPISODES):
@@ -125,7 +143,7 @@ for episode in range(NB_EPISODES):
             reward = FOOD_REWARD
         else:
             reward = -MOVE_PENALTY
-        ## NOW WE KNOW THE REWARD, LET'S CALC YO
+     
         # first we need to obs immediately after the move.
         new_obs = (player-food, player-enemy)
         max_future_q = np.max(q_table[new_obs])
@@ -144,8 +162,8 @@ for episode in range(NB_EPISODES):
             env[enemy.x][enemy.y] = d[ENEMY_ID]  # sets the enemy location to red
             img = Image.fromarray(env, 'RGB')  # reading to rgb. Apparently. Even tho color definitions are bgr. ???
             img = img.resize((300, 300))  # resizing so we can see our agent in all its glory.
-            cv2.imshow("image", np.array(img))  # show it!
-            if reward == FOOD_REWARD or reward == -ENEMY_PENALTY:  # crummy code to hang at the end if we reach abrupt end for good reasons or not.
+            cv2.imshow("image", np.array(img))  # display it
+            if reward == FOOD_REWARD or reward == -ENEMY_PENALTY:  
                 if cv2.waitKey(500) & 0xFF == ord('q'):
                     break
             else:
